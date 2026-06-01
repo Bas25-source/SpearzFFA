6917faf56ca35688c39effdee1038f5701fe70c7
Bas25-source <114758@wolfsbos.nl>
2026-06-01 22:57:14 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+document.addEventListener("mousemove", e => {
+  let dot = document.createElement("div");
+  dot.style.position = "fixed";
+  dot.style.left = e.clientX + "px";
+  dot.style.top = e.clientY + "px";
+  dot.style.width = "6px";
+  dot.style.height = "6px";
+  dot.style.borderRadius = "50%";
+  dot.style.background = "#4da3ff";
+  dot.style.boxShadow = "0 0 10px #4da3ff";
+  document.body.appendChild(dot);
 
+  setTimeout(() => dot.remove(), 400);
+});
